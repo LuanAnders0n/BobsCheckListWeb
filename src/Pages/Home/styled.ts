5a86309285f7180edbe1127cb068970dae9781ae
LuanAnders0n <luan.anderson.001@gmail.com>
2023-07-19 18:48:17 -0300

    mod

--- src/Pages/Home/styled.ts
+++ src/Pages/Home/styled.ts
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 820px;
-  height: 1180px;
   display: flex;
   align-items: center;
   justify-content: center;
